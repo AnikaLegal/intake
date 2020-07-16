1c6f2c53ff10e4f5bb45787f255051ec55c82441
Matthew Segal <mattdsegal@gmail.com>
2020-07-16 11:54:52 +1000

    Remove unused questions

--- src/questions/repairs/forms/preferences.js
+++ src/questions/repairs/forms/preferences.js
@@ -7,21 +7,6 @@ import * as Conditions from '../conditions'
 import { CLIENT_CALL_TIME } from 'questions/generic'
 import type { Field, Form } from 'types'
 
-const CAN_PAY_QUOTE: Field = {
-  name: 'CAN_PAY_QUOTE',
-  displayName: 'You can pay the quote',
-  prompt:
-    'Would you be willing to pay for the repairs yourself and then claim reimbursment from the landlord?',
-  help:
-    'Paying for the repairs yourself and then claiming reimbursement from your landlord is the fastest way to get the problem fixed. However, we understand that many tenants do not want to pay for the repairs themselves.',
-  type: FIELD_TYPES.RADIO_BTN,
-  rules: [rules.isTruthy],
-  options: [
-    { label: 'Yes', value: 'yes' },
-    { label: 'No', value: 'no' },
-  ],
-}
-
 const IS_VCAT_OK: Field = {
   name: 'IS_VCAT_OK',
   rules: [rules.isTruthy],
@@ -48,29 +33,6 @@ const VCAT_AVOID_REASON: Field = {
   type: FIELD_TYPES.TEXTAREA,
 }
 
-const LETTER_ASSERTS_PROCEEDINGS_RIGHT: Field = {
-  rules: [rules.isTruthy],
-  name: 'LETTER_ASSERTS_PROCEEDINGS_RIGHT',
-  displayName:
-    'You would like to mention your right to commence legal proceedings against your landlord in the letter',
-  prompt:
-    'If a letter needs to be sent to your landlord, would you like the letter to say you have the right to commence legal proceedings against your landlord?',
-  help:
-    'If the letter says you have the right to commence legal proceedings, it will place more pressure on the landlord to perform the repairs quickly. If the letter does not say you have the right to commence legal proceedings against your landlord, it will be a more “gentle” letter.',
-
-  type: FIELD_TYPES.RADIO_BTN,
-  options: [
-    {
-      label: 'Yes',
-      value: 'yes',
-    },
-    {
-      label: 'No',
-      value: 'no',
-    },
-  ],
-}
-
 const CLIENT_BREACHED_LEASE: Field = {
   rules: [rules.isTruthy],
   name: 'CLIENT_BREACHED_LEASE',
@@ -89,10 +51,8 @@ export const PERSONAL_PREFERENCES: Form = {
   name: 'PERSONAL_PREFERENCES',
   prompt: 'Tell us what you prefer',
   fields: [
-    CAN_PAY_QUOTE,
     IS_VCAT_OK,
     VCAT_AVOID_REASON,
-    LETTER_ASSERTS_PROCEEDINGS_RIGHT,
     CLIENT_BREACHED_LEASE,
     CLIENT_CALL_TIME,
   ],
