aa259c22e3bb2296143ab1df15e4fafa23c3a6e1
Matt Gleeson <gleesonm@deakin.edu.au>
2022-05-12 10:16:59 +1000

    Just need to make the form now

---
+++ src/design/contact-form.js
@@ -0,0 +1,7 @@
+// @flow
+import React from 'react'
+import { Splash, Text, BigButton } from 'design'
+import { IMAGES } from 'consts'
+import styled from 'styled-components'
+import { useScrollTop } from 'utils'
+
