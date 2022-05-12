20c8b0573e1a01f2890e7ae5511fc8c8b671db3d
Matt Gleeson <gleesonm@deakin.edu.au>
2022-05-13 08:10:31 +1000

    Just need to do the functionality for the form now

--- src/views/splash/noemail.js
+++ src/views/splash/noemail.js
@@ -1,6 +1,6 @@
 // @flow
 import React from 'react'
-import { Splash, Text, BigButton } from 'design'
+import { Splash, Text, BigButton, ContactForm } from 'design'
 import { IMAGES } from 'consts'
 import styled from 'styled-components'
 import { useScrollTop } from 'utils'
@@ -11,16 +11,16 @@ export const NoEmailView = () => {
     <Splash.Container left>
       <Splash.Image src={IMAGES.HEROES.SHRUB_GUY} />
       <Splash.Content>
-        <Text.Header splash>
+        <Text.Header splash style={{ width: '500px' }}>
           Anika Legal is an online service, with the majority of communication
           and advice sent via email.
         </Text.Header>
-        <Text.Body splash>
+        <Text.Body splash style={{ width: '500px' }}>
           If you don't have an email address, please complete the form below and
           we'll call you to see if we're able to help. If not then we'll be able
           to direct you to another organisation.
         </Text.Body>
-        <Text.Body splash>*INSERT FORM HERE*</Text.Body>{' '}
+        <ContactForm></ContactForm>
       </Splash.Content>
     </Splash.Container>
   )
