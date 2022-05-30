038e5ec4ebd421aa32f317d2542d11f3b260a45a
Matt Gleeson <gleesonm@deakin.edu.au>
2022-05-30 21:21:14 +1000

    Evictions section completed

--- src/questions/issues/evictions.js
+++ src/questions/issues/evictions.js
@@ -31,7 +31,7 @@ export const EVICTION_QUESTIONS: Array<Field> = [
     askCondition: isEvictionIssue,
     effect: async (data: Data) => {
       if (data.EVICTIONS_IS_ALREADY_REMOVED) {
-        return ROUTES.INELIGIBLE_ALREADY_REMOVED
+        return ROUTES.INELIGIBLE_EVICTED
       }
     },
     required: true,
@@ -41,27 +41,6 @@ export const EVICTION_QUESTIONS: Array<Field> = [
       { label: 'No', value: false },
     ],
     Prompt: <span>Have you been removed from your home?</span>,
-  },
-  {
-    name: 'EVICTIONS_IS_UNPAID_RENT',
-    stage: 2,
-    askCondition: isEvictionIssue,
-    effect: async (data: Data) => {
-      if (!data.EVICTIONS_IS_UNPAID_RENT) {
-        return ROUTES.INELIGIBLE_NOT_UNPAID_RENT
-      }
-    },
-    required: true,
-    type: FIELD_TYPES.CHOICE_SINGLE,
-    choices: [
-      { label: 'Unpaid rent', value: true },
-      { label: 'Another reason', value: false },
-    ],
-    Prompt: (
-      <span>
-        Are you being evicted due to unpaid rent or some other reason?
-      </span>
-    ),
   },
   {
     name: 'EVICTIONS_HAS_NOTICE',
@@ -103,6 +82,58 @@ export const EVICTION_QUESTIONS: Array<Field> = [
         has given you.
       </span>
     ),
+  },
+  {
+    name: 'EVICTIONS_IS_UNPAID_RENT',
+    stage: 2,
+    askCondition: isEvictionIssue,
+    effect: async (data: Data) => {
+      if (!data.EVICTIONS_IS_UNPAID_RENT) {
+        return ROUTES.INELIGIBLE_WRONG_REASON
+      }
+    },
+    required: true,
+    type: FIELD_TYPES.CHOICE_SINGLE,
+    choices: [
+      { label: 'Unpaid rent', value: true },
+      { label: 'Another reason', value: false },
+    ],
+    Prompt: (
+      <span>
+        Are you being evicted due to unpaid rent or some other reason?
+      </span>
+    ),
+  },
+  {
+    name: 'EVICTIONS_IS_VCAT_DATE',
+    stage: 2,
+    askCondition: isEvictionIssue,
+    required: true,
+    type: FIELD_TYPES.CHOICE_SINGLE,
+    choices: [
+      { label: 'Yes', value: true },
+      { label: 'No', value: false },
+    ],
+    Prompt: (
+      <span>Have you been given a date for an evictions hearing at VCAT? </span>
+    ),
+  },
+  {
+    name: 'EVICTIONS_VCAT_DATE',
+    stage: 2,
+    askCondition: isEvictionIssue,
+    effect: async (data: Data) => {
+      var userDate = Date.parse(data.EVICTIONS_VCAT_DATE)
+      var currentDate = Date.now()
+      var fortnightAway = currentDate + 12096e5 // 14 days in milliseconds
+      if (userDate <= fortnightAway) {
+        return ROUTES.INELIGIBLE_VCAT_HEARING
+      }
+    },
+    required: false,
+    type: FIELD_TYPES.DATE,
+    skipText: 'I do not know when the hearing is yet.',
+    Prompt: <span>What date is the VCAT hearing?</span>,
   },
   {
     name: 'EVICTIONS_NOTICE_VACATE_DATE',
@@ -152,149 +183,30 @@ export const EVICTION_QUESTIONS: Array<Field> = [
     ),
   },
   {
-    name: 'EVICTIONS_DOC_DELIVERY_METHOD_NOTICE_TO_VACATE',
-    stage: 2,
-    askCondition: isEvictionIssue,
-    required: true,
-    type: FIELD_TYPES.CHOICE_SINGLE,
-    choices: [
-      { label: 'Delivered personally', value: 'Delivered personally' },
-      { label: 'By registered post', value: 'By registered post' },
-      { label: 'By email', value: 'By email' },
-      { label: 'Another delivery method', value: 'Another delivery method' },
-    ],
-    Prompt: <span>How did you receive the Notice to Vacate?</span>,
-  },
-  {
-    name: 'EVICTIONS_IS_VCAT_DATE',
-    stage: 2,
-    askCondition: isEvictionIssue,
-    required: true,
-    type: FIELD_TYPES.CHOICE_SINGLE,
-    choices: [
-      { label: 'Yes', value: true },
-      { label: 'No', value: false },
-    ],
-    Prompt: (
-      <span>Have you been given a date for an evictions hearing at VCAT? </span>
-    ),
-  },
-  {
-    name: 'EVICTIONS_VCAT_DATE',
-    stage: 2,
-    askCondition: (data) =>
-      isEvictionIssue(data) && data.EVICTIONS_IS_VCAT_DATE,
-    required: true,
-    type: FIELD_TYPES.DATE,
-    Prompt: <span>What date is the VCAT hearing?</span>,
-  },
-  {
-    name: 'EVICTIONS_RENT_UNPAID',
-    stage: 2,
-    askCondition: isEvictionIssue,
-    required: true,
-    type: FIELD_TYPES.NUMBER,
-    Prompt: <span>How much rent is unpaid?</span>,
-  },
-  {
-    name: 'EVICTIONS_RENT_CYCLE',
-    stage: 2,
-    askCondition: isEvictionIssue,
-    required: true,
-    Prompt: <span>How often do you pay your rent?</span>,
-    type: FIELD_TYPES.CHOICE_SINGLE,
-    choices: [
-      { label: 'Weekly', value: 'WEEKLY' },
-      { label: 'Fortnightly', value: 'FORTNIGHTLY' },
-      { label: 'Monthly', value: 'MONTHLY' },
-      { label: 'Quarterly', value: 'QUATERLY' },
-      { label: 'Other', value: 'OTHER' },
-    ],
-  },
-  {
-    name: 'EVICTIONS_IS_ON_PAYMENT_PLAN',
-    stage: 2,
-    askCondition: isEvictionIssue,
-    required: true,
-    type: FIELD_TYPES.CHOICE_SINGLE,
-    choices: [
-      { label: 'Yes', value: true },
-      { label: 'No', value: false },
-    ],
-    Prompt: (
-      <span>Are you currently on a payment plan with your landlord?</span>
-    ),
-    Help: (
-      <span>
-        This is an agreement where you pay back extra rent to cover the
-        previously unpaid rent.
-      </span>
-    ),
-  },
-  {
-    name: 'EVICTIONS_CAN_AFFORD_PAYMENT_PLAN',
-    stage: 2,
-    askCondition: (data) =>
-      isEvictionIssue(data) && !data.EVICTIONS_IS_ON_PAYMENT_PLAN,
-    required: true,
-    type: FIELD_TYPES.CHOICE_SINGLE,
-    choices: [
-      { label: 'Yes', value: 'YES' },
-      { label: 'No', value: 'NO' },
-      { label: 'Prefer to discuss over phone', value: 'DISCUSS_OVER_PHONE' },
-    ],
-    Prompt: (
-      <span>
-        Would you be able to afford a payment plan with your landlord?
-      </span>
-    ),
-  },
-  // Payment plan stuff
-  {
-    name: 'EVICTIONS_PAYMENT_AMOUNT',
-    stage: 2,
-    askCondition: (data) =>
-      isEvictionIssue(data) &&
-      !data.EVICTIONS_IS_ON_PAYMENT_PLAN &&
-      data.EVICTIONS_CAN_AFFORD_PAYMENT_PLAN == 'YES',
-    required: true,
-    type: FIELD_TYPES.NUMBER,
-    Prompt: (
-      <span>
-        How much additional rent could you pay on top of your normal rent
-        payment?
-      </span>
-    ),
-    Help: <span>This would be to pay off the unpaid rent</span>,
-  },
-  {
     name: 'EVICTIONS_PAYMENT_FAIL_REASON',
     stage: 2,
-    askCondition: (data) =>
-      isEvictionIssue(data) &&
-      !data.EVICTIONS_IS_ON_PAYMENT_PLAN &&
-      data.EVICTIONS_CAN_AFFORD_PAYMENT_PLAN == 'YES',
+    askCondition: isEvictionIssue,
     required: true,
     type: FIELD_TYPES.CHOICE_MULTI,
     Prompt: <span>Why did you fall behind on rent payments?</span>,
     choices: [
       { label: 'Reduced income', value: 'Reduced income' },
       { label: 'Unable to work', value: 'Unable to work' },
-      { label: 'Emotional distress', value: 'Emotional distress' },
+      { label: 'Decline in health', value: 'Decline in health' },
       {
         label: 'Another tenant moving out',
         value: 'Another tenant moving out',
       },
+      { label: 'Family violence', value: 'Family violence' },
+      { label: 'Loss of job', value: 'Loss of job' },
+      { label: 'Struggling financially', value: 'Struggling financially' },
       { label: 'Another reason', value: 'Another reason' },
     ],
   },
   {
     name: 'EVICTIONS_PAYMENT_FAIL_DESCRIPTION',
     stage: 2,
-    askCondition: (data) =>
-      isEvictionIssue(data) &&
-      !data.EVICTIONS_IS_ON_PAYMENT_PLAN &&
-      data.EVICTIONS_CAN_AFFORD_PAYMENT_PLAN == 'YES',
+    askCondition: isEvictionIssue,
     required: false,
     type: FIELD_TYPES.TEXT,
     Prompt: <span>Tell us about why you fell behind on rent payments.</span>,
@@ -302,40 +214,6 @@ export const EVICTION_QUESTIONS: Array<Field> = [
       <span>
         Please provide only as much information as you are comfortable with
         sharing.
-      </span>
-    ),
-  },
-  {
-    name: 'EVICTIONS_PAYMENT_FAIL_CHANGE',
-    stage: 2,
-    askCondition: (data) =>
-      isEvictionIssue(data) &&
-      !data.EVICTIONS_IS_ON_PAYMENT_PLAN &&
-      data.EVICTIONS_CAN_AFFORD_PAYMENT_PLAN == 'YES',
-    required: false,
-    type: FIELD_TYPES.TEXT,
-    Prompt: (
-      <span>
-        What circumstances have changed to allow you to afford the rent?
-      </span>
-    ),
-    Help: (
-      <span>
-        Please provide only as much information as you are comfortable with
-        sharing.
-      </span>
-    ),
-  },
-  {
-    name: 'EVICTIONS_MISC',
-    stage: 2,
-    askCondition: isEvictionIssue,
-    required: false,
-    type: FIELD_TYPES.TEXT,
-    Prompt: (
-      <span>
-        Is there anything else that you would like us to know about your
-        evictions issue?
       </span>
     ),
   },
---
+++ src/views/splash/ineligible-vcat-hearing.js
@@ -0,0 +1,38 @@
+// @flow
+import React from 'react'
+import { Splash, Text, BigButton } from 'design'
+import { IMAGES, LINKS } from 'consts'
+import styled from 'styled-components'
+import { useScrollTop } from 'utils'
+
+export const IneligibleVcatHearingView = () => {
+  useScrollTop()
+  return (
+    <Splash.Container left>
+      <Splash.Image src={IMAGES.HEROES.SHRUB_GUY} />
+      <Splash.Content>
+        <Text.Header splash>
+          Unfortunately our evictions service is focussed on helping renters
+          resist eviction by negotiating a payment plan, and helping you prepare
+          for your VCAT hearing.
+        </Text.Header>
+        {/* <Text.Body splash>
+          You may wish to contact your local community legal centres who may be
+          better placed to look into your matter. We have arranged for their
+          contact details to be sent to your email separately. This should
+          arrive in the next 2 - 3 business days.
+        </Text.Body> */}
+        {/* FOR THE ABOVE WE ARE AWAITING FOR REFERRAL PATHWAY PROCESS */}
+        <Splash.ButtonGroup>
+          <a href={LINKS.HOME}>
+            <Splash.Button last>Return home</Splash.Button>
+          </a>
+        </Splash.ButtonGroup>
+      </Splash.Content>
+    </Splash.Container>
+  )
+}
+
+const SplashButton = styled(BigButton)`
+  margin-top: 50px;
+`
