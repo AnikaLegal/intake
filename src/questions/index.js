c5e6d0399557c5f6addb137e3452f22d70b10f49
Zach Giofkou <85324964+ziggygio@users.noreply.github.com>
2024-04-23 17:58:33 +1000

    TEC-82, TEC-83: Rename final step primary action button and fix to final step stage (#20)
    * update confirmation text from "OK" to "Confirm" and fix to form stage
    
    * remove console.log

--- src/questions/index.js
+++ src/questions/index.js
@@ -7,6 +7,7 @@ import { events } from 'analytics'
 import { api } from 'api'
 import { storeFormData } from 'utils'
 import type { Field, Data } from 'types'
+import { Icon } from 'design'
 
 import { ABOUT_QUESTIONS } from './about'
 import { ISSUE_QUESTIONS } from './issues'
@@ -21,46 +22,48 @@ export const QUESTIONS: Array<Field> = [
   ...ISSUE_QUESTIONS,
   ...PROPERTY_QUESTIONS,
   ...LANDLORD_QUESTIONS,
-  ...IMPACT_QUESTIONS,
-  {
-    name: 'SUBMIT',
-    required: true,
-    stage: 4,
-    type: FIELD_TYPES.DISPLAY,
-    Prompt: (
-      <span>
-        By submitting this form, you are agreeing to our{' '}
-        <a href={LINKS.PRIVACY_POLICY}>Privacy Policy</a>,{' '}
-        <a href={LINKS.COLLECTIONS_STATEMENT}>Collections Statement</a> and
-        website <a href={LINKS.TERMS_OF_USE}>Terms of Use</a>.
-      </span>
-    ),
-    buttonText: 'Confirm',
-    effect: async (data: Data) => {
-      const finalData = { ...data }
-      // Set all unasked questions to null.
-      for (let q of QUESTIONS) {
-        const isUndef = typeof data[q.name] === 'undefined'
-        const isFailCondition = q.askCondition && !q.askCondition(data)
-        if (isUndef || isFailCondition) {
-          finalData[q.name] = null
-        }
+  ...IMPACT_QUESTIONS
+];
+
+const SUBMIT_QUESTIONS: Field = {
+  name: 'SUBMIT',
+  required: true,
+  stage: Math.max(...QUESTIONS.map(question => question.stage)),
+  type: FIELD_TYPES.DISPLAY,
+  Prompt: (
+    <span>
+      By submitting this form, you are agreeing to our{' '}
+      <a href={LINKS.PRIVACY_POLICY}>Privacy Policy</a>,{' '}
+      <a href={LINKS.COLLECTIONS_STATEMENT}>Collections Statement</a> and
+      website <a href={LINKS.TERMS_OF_USE}>Terms of Use</a>.
+    </span>
+  ),
+  button: { text: 'Confirm', Icon: Icon.Tick },
+  effect: async (data: Data) => {
+    const finalData = { ...data }
+    // Set all unasked questions to null.
+    for (let q of QUESTIONS) {
+      const isUndef = typeof data[q.name] === 'undefined'
+      const isFailCondition = q.askCondition && !q.askCondition(data)
+      if (isUndef || isFailCondition) {
+        finalData[q.name] = null
       }
-      console.log('Submitting data:', finalData)
-      const subId = data['id']
-      let sub
-      if (subId) {
-        // We have already created this submission
-        sub = await api.submission.update(subId, finalData)
-      } else {
-        // This is a new submission
-        sub = await api.submission.create(finalData)
-      }
-      await api.submission.submit(sub.id)
-      // Wipe stored data.
-      storeFormData('')
-      events.onFinishIntake()
-      return ROUTES.SUBMITTED
-    },
+    }
+    const subId = data['id']
+    let sub
+    if (subId) {
+      // We have already created this submission
+      sub = await api.submission.update(subId, finalData)
+    } else {
+      // This is a new submission
+      sub = await api.submission.create(finalData)
+    }
+    await api.submission.submit(sub.id)
+    // Wipe stored data.
+    storeFormData('')
+    events.onFinishIntake()
+    return ROUTES.SUBMITTED
   },
-]
+};
+
+QUESTIONS.push(SUBMIT_QUESTIONS);
