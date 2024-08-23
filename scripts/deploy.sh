6fc011363579e57e47ba34acce42c85275ddebed
Luca Vari <luca.vari@gmail.com>
2024-08-23 10:42:53 +1000

    Remove assets from static site that are not included in the source directory
    That is, delete assets that have been removed from the source or the old
    versions of assets that are regenerated with a new name each build - like js,
    css & map bundles.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,13 +5,11 @@ set -e
 # https://docs.sentry.io/cli/releases/#upload-source-maps?platform=javascript#upload-source-maps
 COMMIT_HASH=$(git rev-parse HEAD)
 
-if [ -z "$SENTRY_PROJECT" ]
-then
+if [ -z "$SENTRY_PROJECT" ]; then
     # Do not do anything with Sentry
     echo -e "\n>>> Nothing to upload to Sentry"
 else
-    if [ -z "$SENTRY_AUTH_TOKEN" ]
-    then
+    if [ -z "$SENTRY_AUTH_TOKEN" ]; then
         echo "Error: SENTRY_AUTH_TOKEN must be set"
         exit 1
     fi
@@ -40,23 +38,21 @@ fi
 
 # Compress HTML, JS, CSS
 COMPRESS="html js css"
-for SUFFIX in $COMPRESS
-do
+for SUFFIX in $COMPRESS; do
     FILES=$(find dist/ -name "*.$SUFFIX")
-    for FILE in $FILES
-    do
+    for FILE in $FILES; do
         gzip $FILE
         mv ${FILE}.gz $FILE
     done
 done
 
 CACHE_CONTROL="max-age=86400"
-    echo "\n>>> Uploading assets to S3 bucket $S3_BUCKET_URL"
+echo "\n>>> Uploading assets to S3 bucket $S3_BUCKET_URL"
 
 # Upload HTML with no cache, gzip encoding
 echo -e "\nUploading HTML to $S3_BUCKET_URL"
-aws s3 cp \
-    --recursive \
+aws s3 sync \
+    --delete \
     --content-encoding 'gzip' \
     --exclude '*' \
     --include '*.html' \
@@ -64,12 +60,10 @@ aws s3 cp \
     ./dist \
     $S3_BUCKET_URL
 
-
-
 # Upload CSS + JS with cache and gzip
 echo -e "\nUploading JS + CSS to $S3_BUCKET_URL"
-aws s3 cp \
-    --recursive \
+aws s3 sync \
+    --delete \
     --content-encoding 'gzip' \
     --exclude '*' \
     --include '*.css' \
@@ -81,8 +75,8 @@ aws s3 cp \
 
 # Upload everything else with cache and no gzip
 echo -e "\nUploading other assets to $S3_BUCKET_URL"
-aws s3 cp \
-    --recursive \
+aws s3 sync \
+    --delete \
     --exclude '*.css' \
     --exclude '*.js' \
     --exclude '*.map' \
@@ -93,8 +87,7 @@ aws s3 cp \
     $S3_BUCKET_URL
 
 # Finalize Sentry release
-if [ -z "$SENTRY_PROJECT" ]
-then
+if [ -z "$SENTRY_PROJECT" ]; then
     # Do not do anything with Sentry
     echo -e "\nNothing to finalize with Sentry"
 else
@@ -115,7 +108,6 @@ else
         --project=${SENTRY_PROJECT} \
         deploys $COMMIT_HASH new --env $SENTRY_ENV
 fi
-
 
 # # Clean up so you don't shoot yourself in the foot when doing dev work.
 echo "Cleaning up post build..."
