d312af56dfbc4effaaf5874d7a881ff662aa43de
Luca Vari <luca.vari@gmail.com>
2025-10-14 10:52:26 +1100

    fix: correct uncommon spelling

--- src/views/splash/ineligible-repairs-gotten-vcat.js
+++ src/views/splash/ineligible-repairs-gotten-vcat.js
@@ -13,7 +13,7 @@ export const IneligibleRepairsGottenVCATView = () => {
       <Splash.Image src={IMAGES.HEROES.PAPER_GUY} />
       <Splash.Content>
         <Text.Header splash>
-          Our Repairs service focusses on helping renters write a formal
+          Our Repairs service focuses on helping renters write a formal
           compliance request to their real estate agent and/or rental provider,
           and our service scope ends once a Repairs Order has been obtained.
           Based on what you've told us, you have already obtained a Repairs
